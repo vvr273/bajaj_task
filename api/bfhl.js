00bddec1e77d69c5f531ef60afe05ef340775eac
vvr273 <vvr2732004@gmail.com>
2025-08-29 11:45:53 +0530

    Added api/bfhl endpoint for Vercel

---
+++ api/bfhl.js
@@ -0,0 +1,49 @@
+import express from "express";
+import serverless from "serverless-http";
+
+const app = express();
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const data = req.body.data || [];
+
+  let odd_numbers = [];
+  let even_numbers = [];
+  let alphabets = [];
+  let special_characters = [];
+  let sum = 0;
+
+  data.forEach(item => {
+    if (!isNaN(item) && item.trim() !== "") {
+      let num = parseInt(item, 10);
+      if (num % 2 === 0) even_numbers.push(item);
+      else odd_numbers.push(item);
+      sum += num;
+    } else if (/^[a-zA-Z]+$/.test(item)) {
+      alphabets.push(item.toUpperCase());
+    } else {
+      special_characters.push(item);
+    }
+  });
+
+  let concat_string = [...alphabets]
+    .map(ch => ch.toLowerCase())
+    .reverse()
+    .map((ch, i) => (i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
+    .join("");
+
+  res.json({
+    is_success: true,
+    user_id: "john_doe_17091999",
+    email: "john@xyz.com",
+    roll_number: "ABCD123",
+    odd_numbers,
+    even_numbers,
+    alphabets,
+    special_characters,
+    sum: sum.toString(),
+    concat_string,
+  });
+});
+
+export default serverless(app);
